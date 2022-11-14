15d8a8ef30f9f0c12b6e7815d5c8b47105ad3ad8
AleksandarCumic <acumic11622rn@raf.rs>
2022-11-14 23:49:50 +0100

    dodat message u pozadini factory kostur dodat

--- src/main/java/dsw/gerumap/app/gui/swing/controller/RemoveAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/RemoveAction.java
@@ -2,8 +2,11 @@ package main.java.dsw.gerumap.app.gui.swing.controller;
 
 import main.java.dsw.gerumap.app.gui.swing.tree.model.MapTreeItem;
 import main.java.dsw.gerumap.app.gui.swing.view.MainFrame;
+import main.java.dsw.gerumap.app.message.EventType;
+import main.java.dsw.gerumap.app.message.implementation.MessageGeneratorImplementation;
 
 import javax.swing.*;
+import javax.swing.tree.TreeNode;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
@@ -21,4 +24,7 @@ public class RemoveAction extends AbstractGerumapAction {
         MapTreeItem selected = (MapTreeItem) MainFrame.getInstance().getMapTree().getSelectedNode();
         MainFrame.getInstance().getMapTree().removeChild(selected);
     }
+
+
+
 }
